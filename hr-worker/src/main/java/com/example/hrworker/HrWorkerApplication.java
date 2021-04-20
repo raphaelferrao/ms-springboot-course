0f41487098d37a429e79cea0479335aae8ea307c
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-04-20 17:51:26 -0300

    - configuring Eureka Clients

--- hr-worker/src/main/java/com/example/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/example/hrworker/HrWorkerApplication.java
@@ -5,10 +5,12 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
