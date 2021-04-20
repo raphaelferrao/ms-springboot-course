de17098522925fdd419537458626a7853c6a2251
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-04-20 17:20:16 -0300

    - communication by FeignCient

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -5,10 +5,12 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
