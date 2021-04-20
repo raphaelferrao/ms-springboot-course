d4d01225c19bc2d6404ca734a9913c1585a08e2d
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-04-20 18:47:36 -0300

    - Configuring HR Config Server

--- hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
@@ -1,13 +1,31 @@
 package com.example.hrconfigserver;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void afterStart() {
+		log.info("****** HR-CONFIG-SERVER STARTED ******");
+	}
+	
+	@PreDestroy
+	public void beforeStop() {
+		log.info("****** HR-CONFIG-SERVER STOPPED ******");
 	}
 
 }
