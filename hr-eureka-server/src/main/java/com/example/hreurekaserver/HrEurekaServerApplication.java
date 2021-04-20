28306f3b4fced09b83d626ddafb86f806f72d7d8
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-04-20 17:40:19 -0300

    - configuring Eureka Server

--- hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
@@ -5,10 +5,12 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
