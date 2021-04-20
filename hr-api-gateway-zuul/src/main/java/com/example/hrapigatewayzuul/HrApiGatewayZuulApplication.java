fce67f38d148cd71072865adb160668873d944e4
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-04-20 18:18:21 -0300

    - Configuring Zuul API Gateway

--- hr-api-gateway-zuul/src/main/java/com/example/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/example/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,8 +1,18 @@
 package com.example.hrapigatewayzuul;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
@@ -10,4 +20,14 @@ public class HrApiGatewayZuulApplication {
 		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
 
+	@PostConstruct
+	public void afterStart() {
+		log.info("****** HR-API-GATEWAY-ZUUL STARTED ******");
+	}
+	
+	@PreDestroy
+	public void beforeStop() {
+		log.info("****** HR-API-GATEWAY-ZUUL STOPPED ******");
+	}
+	
 }
