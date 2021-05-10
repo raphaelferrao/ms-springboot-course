c013da63cc78d79947f4cd6b9dc38c47407b1c0d
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2021-05-10 15:00:21 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/example/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/example/hroauth/services/UserService.java
@@ -1,9 +1,14 @@
 package com.example.hroauth.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.example.hroauth.entities.User;
 
 public interface UserService {
 
 	User findByEmail(String email);
 
+	UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+	
 }
--- hr-oauth/src/main/java/com/example/hroauth/services/UserServiceImpl.java
+++ hr-oauth/src/main/java/com/example/hroauth/services/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.hroauth.entities.User;
@@ -10,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -24,7 +27,20 @@ public class UserServiceImpl implements UserService {
 			throw new IllegalArgumentException(String.format("Email %s not found", email));
 		}
 		
-		log.info("Email {} found", email);
+		log.info("Email {} found: {}", email, user);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if (user == null) {
+			log.error("User with email {} not found", username);
+			throw new UsernameNotFoundException(String.format("User with email  %s not found", username));
+		}
+		
+		log.info("User with mail {} found: {}", username, user);
 		return user;
 	}
 
