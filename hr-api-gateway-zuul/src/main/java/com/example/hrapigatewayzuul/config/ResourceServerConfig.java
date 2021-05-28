aa7511d6aac7dad9ed5cd59a88edd37c03bc51f6
Raphael Ferrao <raphael.ferrao@gmail.com>
2021-05-28 18:20:33 -0300

    Cors configuration

--- hr-api-gateway-zuul/src/main/java/com/example/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/example/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,18 +1,36 @@
 package com.example.hrapigatewayzuul.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
+	private static final List<String> CORS_ALLOWED_METHODS = Arrays.asList(
+			HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.DELETE.name(), 
+			HttpMethod.PUT.name(), HttpMethod.PATCH.name());
+	
+	private static final List<String> CORS_ALLOWED_HEADERS = Arrays.asList(
+			HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE);
+
 	private static final String[] PUBLIC_URLS = { "/hr-oauth/oauth/token" };
 	
 	private static final String[] ROLE_OPERATOR_URLS = { "/hr-worker/**" };
@@ -36,8 +54,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, ROLE_OPERATOR_URLS).hasAnyRole("OPERATOR", "ADMIN")
 			.antMatchers(ROLE_ADMIN_URLS).hasAnyRole("ADMIN")
 			.anyRequest().authenticated();
+		
+		http.cors().configurationSource(corsConfigurationSource());
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedOrigins(CORS_ALLOWED_METHODS);
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(CORS_ALLOWED_HEADERS);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return source;
+	}
 	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> filterBean = 
+				new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		filterBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return filterBean;
+	}
 	
 }
--- hr-oauth/src/main/java/com/example/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/com/example/hroauth/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.hroauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/search/**").permitAll();
 	}
 	
 	
